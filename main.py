de0338c64dd1e5d5fc9a7f769663ed395c357103
Ryan <devogelryan@gmail.com>
2020-06-27 14:23:07 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def reverseString(text):
+	return text[::-1]
+
+def multiplySelf(number):
+	return number * number
+
+
+def getHalf(number):
+	return number / 2
+
+print(reverseString("hey"))
+print(multiplySelf(5))
+print(getHalf(12))
